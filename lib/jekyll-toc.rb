a2783ea4e0a92760332c70781ffe3e325a57e3b2
toshi <me@toshimaru.net>
2016-04-17 16:28:37 +0900

    Simplify logic

--- lib/jekyll-toc.rb
+++ lib/jekyll-toc.rb
@@ -4,24 +4,27 @@ require 'table_of_contents/parser'
 module Jekyll
   module TableOfContentsFilter
     def toc_only(html)
-      page = @context.registers[:page]
       return html unless page['toc']
 
-      Jekyll::TableOfContents::Parser.new(html).build_toc
+      ::Jekyll::TableOfContents::Parser.new(html).build_toc
     end
 
     def inject_anchors(html)
-      page = @context.registers[:page]
       return html unless page['toc']
 
-      Jekyll::TableOfContents::Parser.new(html).inject_anchors_into_html
+      ::Jekyll::TableOfContents::Parser.new(html).inject_anchors_into_html
     end
 
     def toc(html)
-      page = @context.registers[:page]
       return html unless page['toc']
 
-      Jekyll::TableOfContents::Parser.new(html).toc
+      ::Jekyll::TableOfContents::Parser.new(html).toc
+    end
+
+    private
+
+    def page
+      @context.registers[:page]
     end
   end
 end
