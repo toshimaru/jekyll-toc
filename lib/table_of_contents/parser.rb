6d3f7d42d5b4218448c37107a62bdc07cf190331
toshimaru <me@toshimaru.net>
2017-04-11 15:46:36 +0900

    Make methods private

--- lib/table_of_contents/parser.rb
+++ lib/table_of_contents/parser.rb
@@ -24,6 +24,49 @@ module Jekyll
         toc << '</ul>'
       end
 
+      def inject_anchors_into_html
+        @entries.each do |entry|
+          entry[:content_node].add_previous_sibling(%(<a id="#{entry[:id]}#{entry[:uniq]}" class="anchor" href="##{entry[:id]}#{entry[:uniq]}" aria-hidden="true"><span class="octicon octicon-link"></span></a>))
+        end
+
+        @doc.inner_html
+      end
+
+      def toc
+        build_toc + inject_anchors_into_html
+      end
+
+      private
+
+      # parse logic is from html-pipeline toc_filter
+      # https://github.com/jch/html-pipeline/blob/v1.1.0/lib/html/pipeline/toc_filter.rb
+      def parse_content
+        entries = []
+        headers = Hash.new(0)
+
+        @doc.css('h1, h2, h3, h4, h5, h6').each do |node|
+          text = node.text
+          id = text.downcase
+          id.gsub!(PUNCTUATION_REGEXP, '') # remove punctuation
+          id.gsub!(' ', '-') # replace spaces with dash
+
+          uniq = headers[id] > 0 ? "-#{headers[id]}" : ''
+          headers[id] += 1
+          header_content = node.children.first
+          next unless header_content
+
+          entries << {
+            id: id,
+            uniq: uniq,
+            text: text,
+            node_name: node.name,
+            content_node: header_content
+          }
+        end
+
+        entries
+      end
+
       # Returns the list items for entries
       def build_lis(entries, min_h_num)
         lis = ''
