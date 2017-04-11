5efa82bd5118aacb685fab765df4fc4e7eb9ae44
toshimaru <me@toshimaru.net>
2017-04-11 15:57:08 +0900

    Use HTML heredoc

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,14 @@ SimpleCov.start
 require 'jekyll'
 require 'minitest/autorun'
 
-SIMPLE_HTML = <<EOL
+SIMPLE_HTML = <<HTML
 <h1>Simple H1</h1>
 <h2>Simple H2</h2>
 <h3>Simple H3</h3>
 <h4>Simple H4</h4>
 <h5>Simple H5</h5>
 <h6>Simple H6</h6>
-EOL
+HTML
 
 module TestHelpers
   def read_html_and_create_parser
