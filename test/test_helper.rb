697eb6c0596035101dbeb2d79c8999a8032c3370
toshimaru <me@toshimaru.net>
2017-10-01 11:26:23 +0900

    Modify heding HTML for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,13 +5,15 @@ require 'minitest/autorun'
 require 'jekyll'
 require 'jekyll-toc'
 
-SIMPLE_HTML = <<-HTML
-<h1>Simple H1</h1>
-<h2>Simple H2</h2>
-<h3>Simple H3</h3>
-<h4>Simple H4</h4>
-<h5>Simple H5</h5>
-<h6>Simple H6</h6>
+SIMPLE_HTML = <<-HTML.freeze
+  <h1>Simple H1</h1>
+  <h2>Simple H2</h2>
+  <h3>Simple H3</h3>
+  <h4>Simple H4</h4>
+  <h5>Simple H5</h5>
+  <h6>Simple H6</h6>
+  <h1>H1 for H6</h1>
+  <h6>H6 from H1</h6>
 HTML
 
 module TestHelpers
