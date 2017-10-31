a1b7ce62eca6d15a996d38b4bd44bf1b288e2895
toshimaru <me@toshimaru.net>
2017-10-31 13:54:22 +0900

    Add toc_enabled? method

--- lib/jekyll-toc.rb
+++ lib/jekyll-toc.rb
@@ -4,24 +4,25 @@ require 'table_of_contents/parser'
 module Jekyll
   module TableOfContentsFilter
     def toc_only(html)
-      return html unless page['toc']
-
+      return html unless toc_enabled?
       ::Jekyll::TableOfContents::Parser.new(html).build_toc
     end
 
     def inject_anchors(html)
-      return html unless page['toc']
-
+      return html unless toc_enabled?
       ::Jekyll::TableOfContents::Parser.new(html).inject_anchors_into_html
     end
 
     def toc(html)
-      return html unless page['toc']
-
+      return html unless toc_enabled?
       ::Jekyll::TableOfContents::Parser.new(html).toc
     end
 
     private
+
+    def toc_enabled?
+      page['toc'] == true
+    end
 
     def page
       @context.registers[:page]
