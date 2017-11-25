bf001ff9affae413d30991c7e738292b03976c48
Toshimaru <me@toshimaru.net>
2017-11-25 23:57:32 +0900

    TOC configuration: configurable heading min_level and max_level (#39)
    * Fix duplicated method name
    
    * Add toc_enabled? method
    
    * Add kramdown heading test
    
    * Create `toc` liqud  custom tag
    
    * Check toc is enabled
    
    * unregister_tag toc
    
    * Add TODO
    
    * Make toc headings dynamic
    
    * Add max_level, min_level test

--- test/test_kramdown_list.rb
+++ test/test_kramdown_list.rb
@@ -1,6 +1,23 @@
 require 'test_helper'
 
 class TestKramdownList < Minitest::Test
+  # NOTE: kramdown automatically injects `id` attribute
+  # TODO: test Japanese heading
+  def test_kramdown_heading
+    text = <<-MARKDOWN
+# h1
+
+## h2
+    MARKDOWN
+    expected = <<-HTML
+<h1 id="h1">h1</h1>
+
+<h2 id=\"h2\">h2</h2>
+    HTML
+
+    assert_equal(expected, Kramdown::Document.new(text).to_html)
+  end
+
   def test_kramdown_list_1
     text = <<-MARKDOWN
 * level-1
@@ -72,12 +89,15 @@ class TestKramdownList < Minitest::Test
 * level-1
     MARKDOWN
     expected = <<-HTML
+<pre><code>  * level-4
+* level-3   * level-2 * level-1
+</code></pre>
     HTML
 
     assert_equal(expected, Kramdown::Document.new(text).to_html)
   end
 
-  def test_kramdown_list_3
+  def test_kramdown_list_4
     text = <<-MARKDOWN
 * level-1
       * level-4
@@ -101,7 +121,7 @@ class TestKramdownList < Minitest::Test
     assert_equal(expected, Kramdown::Document.new(text).to_html)
   end
 
-    def test_kramdown_list_3
+    def test_kramdown_list_5
       text = <<-MARKDOWN
 * level-1
     * level-3
