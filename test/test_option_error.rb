dda37e80c89874d725da3c6a3093eb976955ff55
toshimaru <me@toshimaru.net>
2017-12-30 14:44:32 +0900

    Simplify test

--- test/test_option_error.rb
+++ test/test_option_error.rb
@@ -1,36 +1,38 @@
 require 'test_helper'
 
 class TestOptionError < Minitest::Test
-  def test_option_is_nil
-    parser = Jekyll::TableOfContents::Parser.new("<h1>h1</h1>", nil)
-    doc = Nokogiri::HTML(parser.toc)
-    expected = <<-HTML
+  BASE_HTML = "<h1>h1</h1>"
+  EXPECTED_HTML = <<-HTML
 <ul class="section-nav">
 <li class="toc-entry toc-h1"><a href="#h1">h1</a></li>
 </ul>
-    HTML
+  HTML
+
+  def test_option_is_nil
+    parser = Jekyll::TableOfContents::Parser.new(BASE_HTML, nil)
+    doc = Nokogiri::HTML(parser.toc)
+    expected = EXPECTED_HTML
+    assert_equal(expected, doc.css('ul.section-nav').to_s)
+  end
+
+  def test_option_is_epmty_string
+    parser = Jekyll::TableOfContents::Parser.new(BASE_HTML, "")
+    doc = Nokogiri::HTML(parser.toc)
+    expected = EXPECTED_HTML
     assert_equal(expected, doc.css('ul.section-nav').to_s)
   end
 
   def test_option_is_string
-    parser = Jekyll::TableOfContents::Parser.new("<h1>h1</h1>", "string")
+    parser = Jekyll::TableOfContents::Parser.new(BASE_HTML, "string")
     doc = Nokogiri::HTML(parser.toc)
-    expected = <<-HTML
-<ul class="section-nav">
-<li class="toc-entry toc-h1"><a href="#h1">h1</a></li>
-</ul>
-    HTML
+    expected = EXPECTED_HTML
     assert_equal(expected, doc.css('ul.section-nav').to_s)
   end
 
   def test_option_is_array
-    parser = Jekyll::TableOfContents::Parser.new("<h1>h1</h1>", [])
+    parser = Jekyll::TableOfContents::Parser.new(BASE_HTML, [])
     doc = Nokogiri::HTML(parser.toc)
-    expected = <<-HTML
-<ul class="section-nav">
-<li class="toc-entry toc-h1"><a href="#h1">h1</a></li>
-</ul>
-    HTML
+    expected = EXPECTED_HTML
     assert_equal(expected, doc.css('ul.section-nav').to_s)
   end
 end
