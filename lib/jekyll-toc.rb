b2456faa16f6159353beaf59f5a6677d11011a39
toshimaru <me@toshimaru.net>
2018-10-19 13:55:34 +0900

    RuboCop correction

--- lib/jekyll-toc.rb
+++ lib/jekyll-toc.rb
@@ -4,27 +4,31 @@ require 'nokogiri'
 require 'table_of_contents/parser'
 
 module Jekyll
-  class TocTag < Liquid::Tag
-    def render(context)
-      return unless context.registers[:page]['toc'] == true
-      content_html = context.registers[:page].content
-      ::Jekyll::TableOfContents::Parser.new(content_html).build_toc
-    end
-  end
+  # class TocTag < Liquid::Tag
+  #   def render(context)
+  #     return unless context.registers[:page]['toc']
+  #
+  #     content_html = context.registers[:page].content
+  #     ::Jekyll::TableOfContents::Parser.new(content_html).build_toc
+  #   end
+  # end
 
   module TableOfContentsFilter
     def toc_only(html)
       return html unless toc_enabled?
+
       ::Jekyll::TableOfContents::Parser.new(html, toc_config).build_toc
     end
 
     def inject_anchors(html)
       return html unless toc_enabled?
+
       ::Jekyll::TableOfContents::Parser.new(html, toc_config).inject_anchors_into_html
     end
 
     def toc(html)
       return html unless toc_enabled?
+
       ::Jekyll::TableOfContents::Parser.new(html, toc_config).toc
     end
 
