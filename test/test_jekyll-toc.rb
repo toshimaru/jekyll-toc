2b7df86a160614d321b8eeae394b53d3867bb8e6
Toshimaru <me@toshimaru.net>
2019-03-31 18:51:45 +0900

    Fix toc tag bug / Add tests (#83)
    * Fix typo
    
    * Use Hash access
    
    * Create Jekyll::TableOfContents::Helper module
    
    * Revert "Create Jekyll::TableOfContents::Helper module"
    
    This reverts commit 45649d79e9772302d3462ff75e0f89aa55a80aed.
    
    * Set toc_config
    
    * Add test for coverage

--- test/test_jekyll-toc.rb
+++ test/test_jekyll-toc.rb
@@ -7,19 +7,46 @@ class TestTableOfContentsFilter < Minitest::Test
 
   DUMMY_HTML = '<div>Dummy HTML Content</div>'
 
-  def setup
-    @context = Struct.new(:registers).new(page: { "toc" => false })
-  end
-
   def test_toc_only
+    @context = disable_toc_context
     assert_empty toc_only(DUMMY_HTML)
   end
 
   def test_inject_anchors
-    assert_equal inject_anchors(DUMMY_HTML), DUMMY_HTML
+    @context = disable_toc_context
+    assert_equal DUMMY_HTML, inject_anchors(DUMMY_HTML)
   end
 
   def test_toc
-    assert_equal toc(DUMMY_HTML), DUMMY_HTML
+    @context = disable_toc_context
+    assert_equal DUMMY_HTML, toc(DUMMY_HTML)
+  end
+
+  def test_toc_only2
+    @context = enable_toc_context
+    assert_equal "<ul class=\"section-nav\">\n</ul>", toc_only(DUMMY_HTML)
+  end
+
+  def test_inject_anchors2
+    @context = enable_toc_context
+    assert_equal DUMMY_HTML, inject_anchors(DUMMY_HTML)
+  end
+
+  def test_toc2
+    @context = enable_toc_context
+    assert_equal "<ul class=\"section-nav\">\n</ul>#{DUMMY_HTML}", toc(DUMMY_HTML)
+  end
+
+  private
+
+  def disable_toc_context
+    Struct.new(:registers).new(page: { 'toc' => false })
+  end
+
+  def enable_toc_context
+    Struct.new(:registers).new(
+      page: { 'toc' => true },
+      site: Struct.new(:config).new({ 'toc' => false })
+    )
   end
 end
--- test/test_toc_tag.rb
+++ test/test_toc_tag.rb
@@ -7,11 +7,15 @@ class TestTableOfContentsTag < Minitest::Test
 
   def setup
     @stubbed_context  = Struct.new(:registers)
+    @stubbed_context1 = Struct.new(:config)
     @stubbed_context2 = Struct.new(:toc, :content)
   end
 
   def test_toc_tag
-    context = @stubbed_context.new(page: @stubbed_context2.new({ 'toc' => false }, '<h1>test</h1>'))
+    context = @stubbed_context.new(
+      page: @stubbed_context2.new({ 'toc' => false }, '<h1>test</h1>'), 
+      site: @stubbed_context1.new({ 'toc' => nil })
+    )
     tag = Jekyll::TocTag.parse('toc_tag', '', Tokenizer.new(''), ParseContext.new)
     assert_equal tag.render(context), "<ul class=\"section-nav\">\n<li class=\"toc-entry toc-h1\"><a href=\"#test\">test</a></li>\n</ul>"
   end
