be51dccd98359c0df231ab3b30137d920d711ca0
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-01-18 12:23:42 +0100

    PR94 - codeclimate fixes

--- lib/table_of_contents/parser.rb
+++ lib/table_of_contents/parser.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: true
-
 require 'erb'
+include ERB::Util
 
+# Jekyll Modul, the root of everything
 module Jekyll
-  include ERB::Util
-
+  # Module to wrap the classes for TOC creation
   module TableOfContents
     # Parse html contents and generate table of contents
     class Parser
@@ -27,7 +26,7 @@ module Jekyll
       def inject_anchors_into_html
         @entries.each do |entry|
           # Add id to h-element
-          entry[:header_parent].set_attribute('id', '#{entry[:id]}')
+          entry[:header_parent].set_attribute('id', "#{entry[:id]}")
 
           # Add link icon after text
           entry[:header_content].add_next_sibling(
@@ -35,8 +34,8 @@ module Jekyll
           )
 
           # Add link 'nav to toc'
-          arrToTop = [2, 3]
-          if arrToTop.include?(entry[:h_num])
+          arr_to_top = [2, 3]
+          if arr_to_top.include?(entry[:h_num])
             entry[:header_content].add_next_sibling(
               %(<span style="float: right"><a class="anchor_to_top" href="#toc" aria-hidden="true">&#x21A5;</a></span>)
             )
