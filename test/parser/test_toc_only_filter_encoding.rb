e0cb2ff9360eae1dd5b869957c72deffea73ee5e
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-02-29 13:37:46 +0100

    Create Unit Tests

---
+++ test/parser/test_toc_only_filter_encoding.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'test_helper_encoding'
+
+class TestTOCOnlyFilterEncodingOn < Minitest::Test
+  include TestHelpersEncoding
+
+  def setup
+    read_html_and_create_parser(true)
+  end
+
+  def test_injects_toc_container_with_anchor_id_url_encoded
+    html = @parser.build_toc
+
+    assert_match(/<ul class="section-nav">/, html)
+
+    assert_match(/<a href="#123">/, html)
+    assert_match(/<a href="#simple-h2a">/, html)
+    assert_match(/<a href="#simple-h2b">/, html)
+    assert_match(/<a href="#sub-h3">/, html)
+    assert_match(/<a href="#simple-h3b">/, html)
+    assert_match(/<a href="#simple-h3c-%C3%BC%C3%A4%C3%B6">/, html)
+  end
+
+  def test_does_not_return_content
+    html = @parser.build_toc
+
+    assert_nil(%r{<h1 id="123">Simple H1</h1>} =~ html)
+  end
+
+  class TestTOCOnlyFilterEncodingOff < Minitest::Test
+    include TestHelpersEncoding
+    
+    def setup
+      read_html_and_create_parser(false)
+    end
+
+    def test_injects_toc_container_without_anchor_id_url_encoded
+      html = @parser.build_toc
+  
+      assert_match(/<ul class="section-nav">/, html)
+  
+      assert_match(/<a href="#123">/, html)
+      assert_match(/<a href="#simple-h2a">/, html)
+      assert_match(/<a href="#simple-h2b">/, html)
+      assert_match(/<a href="#sub-h3">/, html)
+      assert_match(/<a href="#simple-h3b">/, html)
+      assert_match(/<a href="#simple-h3c-üäö">/, html)
+    end
+  end
+end
