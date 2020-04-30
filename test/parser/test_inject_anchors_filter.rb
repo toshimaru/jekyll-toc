1fc508b854b3fac8bcda6cc846ed5dc3910947b7
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-04-30 09:57:17 +0200

    Update test_inject_anchors_filter.rb

--- test/parser/test_inject_anchors_filter.rb
+++ test/parser/test_inject_anchors_filter.rb
@@ -12,7 +12,7 @@ class TestInjectAnchorsFilter < Minitest::Test
   def test_injects_anchors_into_content
     html = @parser.inject_anchors_into_html
 
-    assert_match(%r{<a class="anchor" href="#simple-h1" aria-hidden="true"><span.*span></a>Simple H1}, html)
+    assert_match(%r{<a class="anchor" href="#simple-h1" aria-hidden="true">🔗 </a>Simple H1}, html)
   end
 
   def test_does_not_inject_toc
