694e87f5c2a0c0e35cdfd87a50e4efbf494d1794
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-04-30 08:07:46 +0200

    Add config key inject_anchors_content to set the cntent of the anchor

--- lib/table_of_contents/parser.rb
+++ lib/table_of_contents/parser.rb
@@ -23,7 +23,7 @@ module Jekyll
       def inject_anchors_into_html
         @entries.each do |entry|
           entry[:header_content].add_previous_sibling(
-            %(<a class="anchor" href="##{entry[:id]}" aria-hidden="true"><span class="octicon octicon-link"></span></a>)
+            %(<a class="anchor" href="##{entry[:id]}" aria-hidden="true">#{@configuration.inject_anchors_content}</a>)
           )
         end
 
