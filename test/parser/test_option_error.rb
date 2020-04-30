03b7a5de32d95c7c0a8d7394b291737ff2661e1e
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-04-30 17:44:01 +0200

    fix rubocop

--- test/parser/test_option_error.rb
+++ test/parser/test_option_error.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'test_helper'
 
 class TestOptionError < Minitest::Test
--- test/test_kramdown_list.rb
+++ test/test_kramdown_list.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'test_helper'
 
 class TestKramdownList < Minitest::Test
