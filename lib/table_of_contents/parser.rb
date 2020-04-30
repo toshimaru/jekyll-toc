aeea2ba97d2d2db17b7865b99e68859738dd4319
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-04-30 09:08:33 +0200

    Add id to h-elements

--- lib/table_of_contents/parser.rb
+++ lib/table_of_contents/parser.rb
@@ -22,6 +22,9 @@ module Jekyll
 
       def inject_anchors_into_html
         @entries.each do |entry|
+          # Add id to h-element
+          entry[:header_parent].set_attribute('id', "#{entry[:id]}")
+
           entry[:header_content].add_previous_sibling(
             %(<a class="anchor" href="##{entry[:id]}" aria-hidden="true"><span class="octicon octicon-link"></span></a>)
           )
@@ -53,6 +56,7 @@ module Jekyll
             id: suffix_num.zero? ? id : "#{id}-#{suffix_num}",
             text: CGI.escapeHTML(text),
             node_name: node.name,
+            header_parent: node,
             header_content: node.children.first,
             h_num: node.name.delete('h').to_i
           }
